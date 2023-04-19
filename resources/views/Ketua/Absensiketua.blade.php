93856218b41ed5a410eb38a68b330e36e8ecbb80
FrenricoChang <frenricochang@gmail.com>
2023-04-19 15:34:11 +0700

    XtrapageKetua

---
+++ resources/views/Ketua/Absensiketua.blade.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, shrink-to-fit=no"
+    />
+
+    <!-- Bootstrap CSS -->
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
+      integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N"
+      crossorigin="anonymous"
+    />
+
+    <!-- memanggil css di dalam folder css -->
+    <link rel="stylesheet" href="../public/css/Xtrapage.css" />
+    <link rel="stylesheet" href="css/responsive.css" />
+
+    <!-- memanggil swiper js  -->
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css"
+    />
+    <script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>
+
+    <!-- memanggil font awesome  -->
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+      integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
+      crossorigin="anonymous"
+      referrerpolicy="no-referrer"
+    />
+
+    <!-- memanggil aos  -->
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet" />
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+
+    <title></title>
+  </head>
+  <body
+    style="overflow-x: hidden; padding: 0; margin: 0; box-sizing: border-box"
+  >
+    <!-- kalau ingin membuat navbar kita bisa buka web yang namanya botstrap.com lalu cari namanya navbar  -->
+
+    <!-- navbar -->
+
+    <section>
+      <div class="row">
+        <div class="col-lg-8 col-sm-6 col-md-6">
+          <div class="btn_close">
+            <a href="Xtrapage.html" style="text-decoration: none"
+              ><i class="fa-sharp fa-solid fa-arrow-left"></i
+            ></a>
+          </div>
+          <img src="assets/persentasi.png" alt="" />
+        </div>
+        <div class="col-lg-4 col-sm-6 col-md-6">
+          <div class="blank">
+            <p>
+              12:01 - Jevent Natthannael <br />
+              12:05 - Vanessa Kwandinata <br />
+              13:00 - William Suryadharma
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <script
+      src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"
+      integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+      crossorigin="anonymous"
+    ></script>
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct"
+      crossorigin="anonymous"
+    ></script>
+
+    <script>
+      var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        // navigation: {
+        //   nextEl: ".swiper-button-next",
+        //   prevEl: ".swiper-button-prev",
+        // },
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+      });
+
+      AOS.init();
+    </script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
---
+++ resources/views/Ketua/Xtrapageketua.blade.php
@@ -0,0 +1,518 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, shrink-to-fit=no"
+    />
+
+    <!-- Bootstrap CSS -->
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
+      integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N"
+      crossorigin="anonymous"
+    />
+
+    <!-- memanggil css di dalam folder css -->
+    <link rel="stylesheet" href="../public/css/Xtrapage.css" />
+
+    <!-- memanggil swiper js  -->
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css"
+    />
+    <script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>
+
+    <!-- memanggil font awesome  -->
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+      integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
+      crossorigin="anonymous"
+      referrerpolicy="no-referrer"
+    />
+
+    <!-- memanggil aos  -->
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet" />
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+
+    <title></title>
+  </head>
+  <body>
+    <!-- kalau ingin membuat navbar kita bisa buka web yang namanya botstrap.com lalu cari namanya navbar  -->
+
+    <!-- navbar -->
+
+    <!-- navbar -->
+
+    <!-- jumbotron search di botstrap  -->
+
+    <div class="jumbotron jumbotron-fluid">
+      <div class="box-jumbotron">
+        <!-- membuat baris dan kolom di boo tstrap : row untuk membuat baris col untuk collom -->
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-6 col-md-6 col-sm-6" data-aos="fade-right ">
+              <div class="button-elips">
+                <a href="#">Xtra</a>
+                <a href="">Schedule</a>
+                <a href="">Leader</a>
+              </div>
+              <div class="elips">
+                <img src="assets/Ellipse 45.png" alt="" class="elips" style="margin-left: -4.8vw; width: 20.8vw;"/> <!--ganti margin-left-->
+              </div>
+            </div>
+
+            <div
+              class="col-lg-6 col-md-6 col-sm-6"
+              id="bca"
+              data-aos="fade-left"
+            >
+              <center>
+                <img src="assets/bca.png" alt="" class="bca" style="width: 32vw; margin-left: 4.5vw;"/>
+              </center>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <main>
+      <br /><br />
+      <div class="container">
+        <div class="button-make-advance float-right" data-aos="fade-down">
+          <a href="absensi.html" class="btn">Make Attendance</a>
+          <a type="button" class="btn" data-toggle="modal" data-target="#add">
+            Add Schedule
+          </a>
+        </div>
+
+        <br /><br /><br />
+        <!-- membuat navs di botstrap  -->
+        <ul
+          class="nav nav-tabs"
+          id="myTab"
+          role="tablist"
+          data-aos="fade-right"
+        >
+          <li class="nav-item">
+            <a
+              class="nav-link active"
+              id="home-tab"
+              data-toggle="tab"
+              href="#home"
+              role="tab"
+              aria-controls="home"
+              aria-selected="true"
+              >Description</a
+            >
+          </li>
+          <li class="nav-item">
+            <a
+              class="nav-link"
+              id="profile-tab"
+              data-toggle="tab"
+              href="#profile"
+              role="tab"
+              aria-controls="profile"
+              aria-selected="false"
+              >Documentation</a
+            >
+          </li>
+          <li class="nav-item">
+            <a
+              class="nav-link"
+              id="contact-tab"
+              data-toggle="tab"
+              href="#contact"
+              role="tab"
+              aria-controls="contact"
+              aria-selected="false"
+              >Member</a
+            >
+          </li>
+        </ul>
+        <div class="tab-content" id="myTabContent" data-aos="fade-down">
+          <div
+            class="tab-pane fade show active"
+            id="home"
+            role="tabpanel"
+            aria-labelledby="home-tab"
+          >
+            <form action="">
+              <div class="form-group">
+                <label for="exampleFormControlTextarea1">Description :</label>
+                <textarea
+                  class="form-control"
+                  id="exampleFormControlTextarea1"
+                  rows="3"
+                ></textarea>
+              </div>
+
+              <div class="form-group">
+                <label for="exampleFormControlTextarea1">Activity :</label>
+                <textarea
+                  class="form-control"
+                  id="exampleFormControlTextarea1"
+                  rows="3"
+                ></textarea>
+              </div>
+            </form>
+          </div>
+          <div
+            class="tab-pane fade"
+            id="profile"
+            role="tabpanel"
+            aria-labelledby="profile-tab"
+          >
+            <div class="swiper mySwiper">
+              <div class="swiper-wrapper">
+                <div class="swiper-slide">
+                  <div class="card" style="width: 18rem">
+                    <img
+                      src="assets/foto/1.png"
+                      class="card-img-top"
+                      alt="..."
+                    />
+                  </div>
+                </div>
+                <div class="swiper-slide">
+                  <div class="card" style="width: 18rem">
+                    <img
+                      src="assets/foto/2.png"
+                      class="card-img-top"
+                      alt="..."
+                    />
+                  </div>
+                </div>
+                <div class="swiper-slide">
+                  <div class="card" style="width: 18rem">
+                    <img
+                      src="assets/foto/3.png"
+                      class="card-img-top"
+                      alt="..."
+                    />
+                  </div>
+                </div>
+                <div class="swiper-slide">
+                  <div class="card" style="width: 18rem">
+                    <img
+                      src="assets/foto/1.png"
+                      class="card-img-top"
+                      alt="..."
+                    />
+                  </div>
+                </div>
+                <div class="swiper-slide">
+                  <div class="card" style="width: 18rem">
+                    <img
+                      src="assets/foto/1.png"
+                      class="card-img-top"
+                      alt="..."
+                    />
+                  </div>
+                </div>
+                <div class="swiper-slide">
+                  <div class="card" style="width: 18rem">
+                    <img
+                      src="assets/foto/1.png"
+                      class="card-img-top"
+                      alt="..."
+                    />
+                  </div>
+                </div>
+                <div class="swiper-slide">
+                  <div class="card" style="width: 18rem">
+                    <img
+                      src="assets/foto/1.png"
+                      class="card-img-top"
+                      alt="..."
+                    />
+                  </div>
+                </div>
+                <div class="swiper-slide">
+                  <div class="card" style="width: 18rem">
+                    <img
+                      src="assets/foto/1.png"
+                      class="card-img-top"
+                      alt="..."
+                    />
+                  </div>
+                </div>
+              </div>
+              <!-- <div class="swiper-pagination"></div> -->
+              <!-- <div class="swiper-button-next"></div>
+              <div class="swiper-button-prev"></div> -->
+            </div>
+          </div>
+          <div
+            class="tab-pane fade"
+            id="contact"
+            role="tabpanel"
+            aria-labelledby="contact-tab"
+          >
+            <div class="row" id="member">
+              <br />
+              <div class="col-lg-6 col-sm-6 col-md-6">
+                <div class="card">
+                  <h4 class="text-center text-dark font-weight-bold">
+                    Member List
+                  </h4>
+
+                  <span class="badge">Jevent Natthannael</span>
+                  <span class="badge">Jordan Cornelius</span>
+                  <span class="badge">Jevent Natthannael</span>
+                  <span class="badge">Jevent Natthannael</span>
+                  <span class="badge">Michael Apen</span>
+                  <span class="badge">Harris Wahyudi</span>
+                </div>
+              </div>
+
+              <br />
+              <div class="col-lg-6 col-sm-6 col-md-6">
+                <img src="assets/stop.png" alt="" class="gambarstop"/>
+                <div class="btn-member">
+                  <button
+                    type="button"
+                    class="btn"
+                    data-toggle="modal"
+                    data-target="#staticBackdrop"
+                  >
+                    Leave xtra
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- prece mmeber list -->
+
+      <div class="presence" data-aos="fade-down">
+        <div class="container">
+          <h5 class="float-right font-weight-bold">Presence Member : 10</h5>
+          <div class="dropdown">
+            <a
+              class="btn dropdown-toggle"
+              href="#"
+              role="button"
+              id="dropdownMenuLink"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              Choose date
+            </a>
+
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+              <a class="dropdown-item" href="#">March 12, 2023</a>
+              <a class="dropdown-item" href="#">March 12, 2023</a>
+              <a class="dropdown-item" href="#">March 12, 2023</a>
+              <a class="dropdown-item" href="#">March 12, 2023</a>
+              <a class="dropdown-item" href="#">March 12, 2023</a>
+            </div>
+          </div>
+
+          <div class="presence-list">
+            <div class="container">
+              <div class="row p-5 d-flex justify-content-center">
+                <div class="col-12">
+                  <h4 class="text-center text-light font-weight-bold">
+                    Presence Member List
+                  </h4>
+
+                  <span class="badge">Jevent Natthannael</span>
+                  <span class="badge">Jordan Cornelius</span>
+                  <span class="badge">Nathaniel Calvin</span>
+                  <span class="badge">Steven Felizion</span>
+                  <span class="badge">Michael Apen</span>
+                  <span class="badge">Harris Wahyudi</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+
+    <!-- modal -->
+    <div
+      class="modal fade"
+      id="staticBackdrop"
+      data-backdrop="static"
+      data-keyboard="false"
+      tabindex="-1"
+      aria-labelledby="staticBackdropLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog modal-dialog-centered">
+        <div
+          class="modal-content"
+          style="border-radius: 20px; border: 2px solid black"
+        >
+          <div class="modal-header" style="border: none">
+            <button
+              type="button"
+              class="close"
+              data-dismiss="modal"
+              aria-label="Close"
+            >
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <h4 class="font-weight-bold">
+              You Will <span class="text-danger">Leave</span> From This Xtra.
+            </h4>
+            <h4 class="font-weight-bold">Do You Want To Continue?</h4>
+
+            <div class="btn-yes-or-close float-right">
+              <button type="button" class="btn btn-success" id="btn_notif">
+                Yes
+              </button>
+              <a href="" class="btn btn-danger">Cancel</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- modal add scadul -->
+    <div
+      class="modal fade"
+      id="add"
+      data-backdrop="static"
+      data-keyboard="false"
+      tabindex="-1"
+      aria-labelledby="staticBackdropLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog modal-dialog-centered modal-xl">
+        <div class="modal-content" style="background-color: #1b2f45">
+          <div class="modal-header" style="border: none">
+            <button
+              type="button"
+              class="close"
+              data-dismiss="modal"
+              aria-label="Close"
+            >
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <div class="row">
+              <div class="col-lg-6 col-sm-6 col-md-6">
+                <input
+                  type="date"
+                  class="form-control"
+                  id="inputEmail3"
+                  style="background-color: #d9d9d9"
+                />
+              </div>
+              <div class="col-lg-6 col-sm-6 col-md-6">
+                <form class="form_add">
+                  <div class="form-group row">
+                    <label for="inputEmail3" class="col-sm-2 col-form-label"
+                      >Xtra:</label
+                    >
+                    <div class="col-sm-10">
+                      <input
+                        type="email"
+                        class="form-control"
+                        id="inputEmail3"
+                      />
+                    </div>
+                  </div>
+                  <div class="form-group row">
+                    <label for="inputEmail3" class="col-sm-2 col-form-label"
+                      >Activity:</label
+                    >
+                    <div class="col-sm-10">
+                      <input
+                        type="email"
+                        class="form-control"
+                        id="inputEmail3"
+                      />
+                    </div>
+                  </div>
+                  <div class="form-group row">
+                    <label for="inputEmail3" class="col-sm-2 col-form-label"
+                      >Schedule:</label
+                    >
+                    <div class="col-sm-10">
+                      <input
+                        type="email"
+                        class="form-control"
+                        id="inputEmail3"
+                      />
+                    </div>
+                  </div>
+                  <div class="form-group row">
+                    <label for="inputEmail3" class="col-sm-2 col-form-label"
+                      >Location:</label
+                    >
+                    <div class="col-sm-10">
+                      <input
+                        type="email"
+                        class="form-control"
+                        id="inputEmail3"
+                      />
+                    </div>
+                  </div>
+
+                  <center>
+                    <button class="btn" id="btn-confirm">Confirm</button>
+                  </center>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- footer -->
+
+    <!-- </footer> -->
+
+    <script
+      src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"
+      integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+      crossorigin="anonymous"
+    ></script>
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct"
+      crossorigin="anonymous"
+    ></script>
+
+    <script>
+      var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        // navigation: {
+        //   nextEl: ".swiper-button-next",
+        //   prevEl: ".swiper-button-prev",
+        // },
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+      });
+
+      AOS.init();
+    </script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
