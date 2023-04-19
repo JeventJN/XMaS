0984d60ae9a52085bd0227b5bba74749cd780036
VanessaKwan <vanessakwan17@gmail.com>
2023-04-19 22:46:14 +0700

    Create Database and Tables

---
+++ database/migrations/2023_04_19_153728_create_schedules_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('schedules', function (Blueprint $table) {
+            $table->increments('kdSchedule');
+            $table->integer('kdExtracurricular');
+            $table->date('date');
+            $table->time('timeStart');
+            $table->time('timeEnd');
+            $table->string('location');
+            $table->text('explanation');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('schedules');
+    }
+};
---
+++ database/migrations/2023_04_19_154141_create_reports_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reports', function (Blueprint $table) {
+            $table->increments('kdReport');
+            $table->integer('kdSchedule');
+            $table->integer('kdState');
+            $table->string('title');
+            $table->text('explanation');
+            $table->string('photo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reports');
+    }
+};
