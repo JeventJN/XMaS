d5801ed3859839d0885146795ca9dbaba1984de1
JeventJN <jeventjn@gmail.com>
2023-05-04 12:30:05 +0700

    FixXtraList

--- resources/views/xtralist.blade.php
+++ resources/views/xtralist.blade.php
@@ -1,13 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
-  <link href="https://fonts.googleapis.com/css2?family=Noto+Serif&display=swap" rel="stylesheet">
-  <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200&display=swap" rel="stylesheet">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="css/Non-User/xtralistNU.css">
-  <title>Xtra List</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Serif&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;0,1000;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900;1,1000&display=swap" rel="stylesheet">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/Non-User/xtralistNU.css">
+    <title>Xtra List</title>
   @vite('resources/css/app.css')
 </head>
 <body class="overflow-x-hidden">
@@ -21,9 +22,9 @@
             @if (request('search'))
                     <input type="hidden" name="search" value={{ request('search') }}>
             @endif
-            <svg xmlns="http://www.w3.org/2000/svg" id="hidemodal" class="mt-[1vw] ml-[43vw] svg cursor-pointer" viewBox="0 0 256 256"><path fill="currentColor" d="M208.49 191.51a12 12 0 0 1-17 17L128 145l-63.51 63.49a12 12 0 0 1-17-17L111 128L47.51 64.49a12 12 0 0 1 17-17L128 111l63.51-63.52a12 12 0 0 1 17 17L145 128Z"/></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" id="hidemodal" class="mt-[1.5vw] ml-[43vw] w-[2vw] h-[2vw] cursor-pointer" viewBox="0 0 256 256"><path fill="currentColor" d="M208.49 191.51a12 12 0 0 1-17 17L128 145l-63.51 63.49a12 12 0 0 1-17-17L111 128L47.51 64.49a12 12 0 0 1 17-17L128 111l63.51-63.52a12 12 0 0 1 17 17L145 128Z"/></svg>
             <div class="flex flex-col ml-[4vw]">
-                <p class="text-[2vw]">Categories</p>
+                <p class="text-[2vw] font-semibold">Categories</p>
                 <div class="flex items-center">
                     <input type="checkbox" id="Physique" name="Physique" value="Physique" class="checkbox w-[1.5vw] h-[1.5vw] underline italic cursor-pointer" {{ (request('Physique') === NULL) ? '' : 'checked' }} >
                     <label class="ml-[1vw] text-[2vw]" for="Physique">Physique</label>
@@ -32,21 +33,21 @@
                     <input type="checkbox" id="NonPhysique" name="NonPhysique" value="NonPhysique" class="checkbox w-[1.5vw] h-[1.5vw] underline italic cursor-pointer" {{ (request('NonPhysique') === NULL) ? '' : 'checked' }} >
                     <label class="ml-[1vw] text-[2vw]" for="NonPhysique">Non-Physique</label>
                 </div>
-                <p class="mt-[1vw] text-[2vw]">Days</p>
-                <div class="flex flex-wrap w-[32vw]">
+                <p class="mt-[1vw] text-[2vw] font-semibold">Days</p>
+                <div class="flex flex-wrap w-[40vw]">
                     <div class="flex items-center w-[8vw] h-[3vw]">
                         <input type="checkbox" id="Mon" name="Mon" value="Mon" class="checkbox w-[1.5vw] h-[1.5vw] underline italic cursor-pointer" {{ (request('Mon') === NULL) ? '' : 'checked' }}>
                         <label class="ml-[1vw] text-[2vw]" for="Mon">Mon</label>
                     </div>
-                    <div class="flex items-center w-[8vw] h-[3vw]">
+                    <div class="flex items-center w-[8vw] h-[3vw] ml-[2vw]">
                         <input type="checkbox" id="Tue" name="Tue" value="Tue" class="checkbox w-[1.5vw] h-[1.5vw] underline italic cursor-pointer"  {{ (request('Tue') === NULL) ? '' : 'checked' }}>
                         <label class="ml-[1vw] text-[2vw]" for="Tue">Tue</label>
                     </div>
-                    <div class="flex items-center w-[8vw] h-[3vw]">
+                    <div class="flex items-center w-[8vw] h-[3vw] ml-[2vw]">
                         <input type="checkbox" id="Wed" name="Wed" value="Wed" class="checkbox w-[1.5vw] h-[1.5vw] underline italic cursor-pointer"  {{ (request('Wed') === NULL) ? '' : 'checked' }}>
                         <label class="ml-[1vw] text-[2vw]" for="Wed">Wed</label>
                     </div>
-                    <div class="flex items-center w-[8vw] h-[3vw]">
+                    <div class="flex items-center w-[8vw] h-[3vw] ml-[2vw]">
                         <input type="checkbox" id="Thu" name="Thu" value="Thu" class="checkbox w-[1.5vw] h-[1.5vw] underline italic cursor-pointer"  {{ (request('Thu') === NULL) ? '' : 'checked' }}>
                         <label class="ml-[1vw] text-[2vw]" for="Thu">Thu</label>
                     </div>
@@ -54,18 +55,18 @@
                         <input type="checkbox" id="Fri" name="Fri" value="Fri" class="checkbox w-[1.5vw] h-[1.5vw] ml-[-0.7vw] underline italic cursor-pointer" {{ (request('Fri') === NULL) ? '' : 'checked' }}>
                         <label class="ml-[1vw] text-[2vw]" for="Fri">Fri</label>
                     </div>
-                    <div class="flex items-center w-[8vw] h-[3vw]">
+                    <div class="flex items-center w-[8vw] h-[3vw] ml-[2vw]">
                         <input type="checkbox" id="Sat" name="Sat" value="Sat" class="checkbox w-[1.5vw] h-[1.5vw] ml-[-0.7vw] underline italic cursor-pointer" {{ (request('Sat') === NULL) ? '' : 'checked' }}>
                         <label class="ml-[1vw] text-[2vw]" for="Sat">Sat</label>
                     </div>
-                    <div class="flex items-center w-[8vw] h-[3vw]">
+                    <div class="flex items-center w-[8vw] h-[3vw] ml-[2vw]">
                         <input type="checkbox" id="Sun" name="Sun" value="Sun" class="checkbox w-[1.5vw] h-[1.5vw] ml-[-0.7vw] underline italic cursor-pointer" {{ (request('Sun') === NULL) ? '' : 'checked' }}>
                         <label class="ml-[1vw] text-[2vw]" for="Sun">Sun</label>
                     </div>
                 </div>
             </div>
             <div class="flex justify-end mr-[2vw] text-[2vw]">
-                <button type="submit">
+                <button type="submit" class="hover:text-[#A1A9B2]">
                     Apply
                 </button>
             </div>
@@ -73,8 +74,8 @@
     </div>
     <div class="bigcontainer">
         <div class="container mt-[9vw] w-[87vw]">
-            <div class="headercontainer flex justify-start items-center mb-[3vw] ml-[5vw]">
-                <p class="text-[1.7vw] font-semibold ml-[1vw]">Xtra List</p>
+            <div class="headercontainer flex justify-start items-center mt-[1.5vw] ml-[5vw]">
+                <p class="text-[2vw] font-semibold ml-[1vw]">Xtra List</p>
                 <button class="svg ml-[39vw] cursor-pointer">
                     <svg xmlns="http://www.w3.org/2000/svg"  id="showmodal" viewBox="0 0 24 24"><path fill="currentColor" d="M9 5a1 1 0 1 0 0 2a1 1 0 0 0 0-2zM6.17 5a3.001 3.001 0 0 1 5.66 0H19a1 1 0 1 1 0 2h-7.17a3.001 3.001 0 0 1-5.66 0H5a1 1 0 0 1 0-2h1.17zM15 11a1 1 0 1 0 0 2a1 1 0 0 0 0-2zm-2.83 0a3.001 3.001 0 0 1 5.66 0H19a1 1 0 1 1 0 2h-1.17a3.001 3.001 0 0 1-5.66 0H5a1 1 0 1 1 0-2h7.17zM9 17a1 1 0 1 0 0 2a1 1 0 0 0 0-2zm-2.83 0a3.001 3.001 0 0 1 5.66 0H19a1 1 0 1 1 0 2h-7.17a3.001 3.001 0 0 1-5.66 0H5a1 1 0 1 1 0-2h1.17z"/></svg>
                 </button>
@@ -126,21 +127,23 @@
                         {{-- @dd($xtra->latest_schedule) --}}
                         <a href="/xtralist/{{ $xtra->kdExtracurricular }}">
                             <div class="xtraboxcontainer flex justify-center items-center">
-                                <div class="xtrabox flex justify-center items-center">
+                                <div class="mr-[0.5vw] xtrabox flex justify-center items-center">
                                     {{-- <img src="{{ $xtra->logo }}" alt="{{ $xtra->name }}"> --}}
                                     <img src="/Assets/{{ $xtra->logo }}" alt="{{ $xtra->name }}">
                                 </div>
-                                <div class="xtrabox flex flex-col items-start justify-center font-nunito">
-                                    <div class="text-[1.7vw] underline font-extrabold mb-[1vw]">{{ $xtra->name }}</div>
-                                    <div class="text-[1.7vw] font-semibold mb-[0.5vw]">{{ optional(optional($xtra->leader)->userXmas)->name }}</div>
-                                    @if ($xtra->leader === NULL)
-                                        <div class="text-[1.7vw] font-semibold mb-[0.5vw]">No Leader Yet</div>
-                                    @endif
-                                    <div class="text-[1.7vw] font-semibold mb-[0.5vw]">{{ date('D', strtotime($xtra->latest_schedule?->date)) . ', ' . date('H.i', strtotime($xtra->latest_schedule?->timeStart)) . ' - ' . date('H.i', strtotime($xtra->latest_schedule?->timeEnd)) }}</div>
-                                    <div class="text-[1.7vw] font-semibold mb-[0.5vw]">{{ $xtra->latest_schedule?->location }}</div>
-                                    @if ($xtra->latest_schedule === NULL)
-                                        <div class="text-[1.7vw] font-semibold mb-[0.5vw]">No Schedule Yet</div>
-                                    @endif
+                                <div class="ml-[0.5vw] xtrabox flex flex-col items-start justify-center font-nunito leading-[3vw]">
+                                    <div class="text-[1.9vw] font-bold underline mb-[1vw]">{{ $xtra->name }}</div>
+                                    <div class="leading-[2vw] text-[1.65vw] font-semibold">
+                                        <div class="text-[1.6vw] font-semibold mb-[0.5vw]">{{ optional(optional($xtra->leader)->userXmas)->name }}</div>
+                                        @if ($xtra->leader === NULL)
+                                            <div class="text-[1.6vw] font-semibold mb-[0.5vw]">No Leader Yet</div>
+                                        @endif
+                                        <div class="text-[1.6vw] font-semibold mb-[0.5vw]">{{ date('D', strtotime($xtra->latest_schedule?->date)) . ', ' . date('H.i', strtotime($xtra->latest_schedule?->timeStart)) . ' - ' . date('H.i', strtotime($xtra->latest_schedule?->timeEnd)) }}</div>
+                                        <div class="text-[1.6vw] font-semibold mb-[0.5vw]">{{ $xtra->latest_schedule?->location }}</div>
+                                        @if ($xtra->latest_schedule === NULL)
+                                            <div class="text-[1.6vw] font-semibold mb-[0.5vw]">No Schedule Yet</div>
+                                        @endif
+                                    </div>
 
                                     {{-- <div class="text-[1.7vw] underline font-extrabold mb-[1vw]">{{ $xtra->name }}</div>
                                     <div class="text-[1.7vw] font-semibold mb-[0.5vw]">{{ $xtra->leader?->userXmas?->name }}</div>
@@ -156,9 +159,15 @@
                         </a>
                     @endforeach
                 @else
-                    <p class="text-center text-[1.7vw] font-semibold mb-[3vw]">No Extracurricular.</p>
+                    <p class="text-center text-[1.7vw] flex justify-center items-center font-semibold mb-[3vw] h-[18vw]">No Extracurricular.</p>
                 @endif
             </div>
         </div>
     </div>
-div>
+    <div class="h-[4vw]"></div>
+    <p id="valueList"></p>
+    <script src="js/xtralist.js"></script>
+    {{-- Footer --}}
+    {{-- @include('footer') --}}
+</body>
+</html>
