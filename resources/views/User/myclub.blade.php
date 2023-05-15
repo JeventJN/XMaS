fa3f48fd44a08021316d2f423bc41b2c54e650ae
Caudrey <103728671+Caudrey@users.noreply.github.com>
2023-05-15 15:28:31 +0700

    myclub done

--- resources/views/User/myclub.blade.php
+++ resources/views/User/myclub.blade.php
@@ -29,115 +29,81 @@
                 <p class="text-[2vw] ml-[1vw] font-bold font-nunito">My Club</p>
             </div>
             <div class="rowcontainer">
-                {{-- TEMPLATE SENGAJA GAK KU HAPUS IN CASE KALIAN MO PAKAI LAGI --}}
                 @if ($xtras->count())
                     @foreach ($xtras as $xtra)
                         {{-- @dd($xtra->latest_schedule) --}}
-                        <a href="/xtralist/{{ $xtra->kdExtracurricular }}">
-                        {{-- <a href="/xtralist"> --}}
-                            <div class="flex flex-col">
-                                <div class="xtraboxcontainer flex justify-center items-center">
-                                    <div class="mr-[0.5vw] xtrabox flex justify-center items-center">
-                                        <img src="{{asset('Assets/$xtra->logo') }}" alt="{{ $xtra->name }}">
-                                        {{-- <img src="{{asset('Assets/RunningLogo.png')}}" alt=""> --}}
-                                    </div>
-                                    <div class="ml-[0.5vw] xtrabox flex flex-col items-start justify-center font-nunito leading-[3vw]">
-                                        <div class="text-[1.9vw] font-bold underline mb-[1vw]">{{ $xtra->name }}</div>
-                                        <div class="leading-[2vw] text-[1.65vw] font-semibold">
-                                            <div class="text-[1.6vw] font-semibold mb-[0.5vw]">{{ implode(' ', array_slice(explode(' ', optional(optional($xtra->leader)->userXmas)->name), 0, 2)) }}</div>
-                                            @if ($xtra->leader === NULL)
-                                                <div class="text-[1.6vw] font-semibold mb-[0.5vw]">No Leader Yet</div>
-                                            @endif
-                                            <div class="text-[1.6vw] font-semibold mb-[0.5vw]">{{ date('D', strtotime($xtra->latest_schedule?->date)) . ', ' . date('H.i', strtotime($xtra->latest_schedule?->timeStart)) . ' - ' . date('H.i', strtotime($xtra->latest_schedule?->timeEnd)) }}</div>
-                                            <div class="text-[1.6vw] font-semibold mb-[0.5vw]">{{ $xtra->latest_schedule?->location }}</div>
-                                            @if ($xtra->latest_schedule === NULL)
-                                                <div class="text-[1.6vw] font-semibold mb-[0.5vw]">No Schedule Yet</div>
-                                            @endif
-                                        </div>
-
-                                        {{-- <div class="text-[1.7vw] underline font-extrabold mb-[1vw]">{{ $xtra->name }}</div>
-                                        <div class="text-[1.7vw] font-semibold mb-[0.5vw]">{{ $xtra->leader?->userXmas?->name }}</div>
-                                        <div class="text-[1.7vw] font-semibold mb-[0.5vw]">{{ date('D', strtotime($xtra->latest_schedule?->date)) . ',' . date('H.i', strtotime($xtra->latest_schedule?->timeStart)) . '-' . date('H.i', strtotime($xtra->latest_schedule?->timeEnd)) }}</div>
-                                        <div class="text-[1.7vw] font-semibold mb-[0.5vw]">{{ ($xtra->latest_schedule ? $xtra->latest_schedule->location : null) ?? 'No Schedule Yet' }}</div> --}}
-                                        {{-- <div class="text-[1.9vw] underline font-extrabold mb-[1vw]">Running</div>
-                                        <div class="leading-[2vw] text-[1.65vw] font-semibold">
-                                            <div class="text-[1.6vw] font-semibold mb-[0.5vw]">Jevent</div>
-                                            <div class="text-[1.6vw] font-semibold mb-[0.5vw]">Wed, 17.00 - 19.00</div>
-                                            <div class="text-[1.6vw] font-semibold mb-[0.5vw]">RTB</div>
-                                        </div> --}}
-                                    </div>
-                                </div>
-                                <div class="absolute w-fit h-fit flex justify-end mt-[18vw] ml-[33vw]">
-                                    <div class="w-[2.5vw] h-[2.5vw]">
-                                        {{-- TEMBAK LINK CHAT DISINI --}}
-                                        <a href="">
-                                            <img class=" w-[2.5vw] h-[2.5vw] hover:scale-[1.1]" src="{{asset('Assets/chat.png')}}" alt="">
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </a>
-                        {{ optional(optional($xtra->leader)->userXmas)->name }}
+                        {{-- {{ optional(optional($xtra->leader)->userXmas)->name }} --}}
                         {{-- {{ $xtra->leader?->NIP . '...' . $nip }} --}}
-                        @if($xtra->leader?->NIP == $nip)
-                        {{-- INI TEMPLATE KALAU KETUA (ADA EDITNYA) --}}
-                        <a href="/xtralist">
-                            <div class="flex flex-col">
-                                <div class="xtraboxcontainer flex justify-center items-center">
-                                    <div class="mr-[0.5vw] xtrabox flex justify-center items-center">
-                                        <img src="{{asset('Assets/RunningLogo.png')}}" alt="">
+                        <a href="/xtralist/{{ $xtra->kdExtracurricular }}">
+                            @if($xtra->leader?->NIP == $nip)
+                                {{-- INI TEMPLATE KALAU KETUA (ADA EDITNYA) --}}
+                                <div class="flex flex-col">
+                                    <div class="xtraboxcontainer flex justify-center items-center">
+                                        <div class="mr-[0.5vw] xtrabox flex justify-center items-center">
+                                            <img src="{{asset('Assets/$xtra->logo') }}" alt="{{ $xtra->name }}">
+                                        </div>
+                                        <div class="ml-[0.5vw] xtrabox flex flex-col items-start justify-center font-nunito leading-[3vw]">
+                                            <div class="text-[1.9vw] underline font-extrabold mb-[1vw]">{{ $xtra->name }}</div>
+                                            <div class="leading-[2vw] text-[1.65vw] font-semibold">
+                                                <div class="text-[1.6vw] font-semibold mb-[0.5vw]">{{ implode(' ', array_slice(explode(' ', optional(optional($xtra->leader)->userXmas)->name), 0, 2)) }}</div>
+                                                @if ($xtra->leader === NULL)
+                                                    <div class="text-[1.6vw] font-semibold mb-[0.5vw]">No Leader Yet</div>
+                                                @endif
+                                                <div class="text-[1.6vw] font-semibold mb-[0.5vw]">{{ date('D', strtotime($xtra->latest_schedule?->date)) . ', ' . date('H.i', strtotime($xtra->latest_schedule?->timeStart)) . ' - ' . date('H.i', strtotime($xtra->latest_schedule?->timeEnd)) }}</div>
+                                                <div class="text-[1.6vw] font-semibold mb-[0.5vw]">{{ $xtra->latest_schedule?->location }}</div>
+                                                @if ($xtra->latest_schedule === NULL)
+                                                    <div class="text-[1.6vw] font-semibold mb-[0.5vw]">No Schedule Yet</div>
+                                                @endif
+                                            </div>
+                                        </div>
                                     </div>
-                                    <div class="ml-[0.5vw] xtrabox flex flex-col items-start justify-center font-nunito leading-[3vw]">
-                                        <div class="text-[1.9vw] underline font-extrabold mb-[1vw]">Running</div>
-                                        <div class="leading-[2vw] text-[1.65vw] font-semibold">
-                                            <div class="text-[1.6vw] font-semibold mb-[0.5vw]">Zakaria</div>
-                                            <div class="text-[1.6vw] font-semibold mb-[0.5vw]">Wed, 17.00 - 19.00</div>
-                                            <div class="text-[1.6vw] font-semibold mb-[0.5vw]">RTB</div>
+                                    <div class="absolute w-fit h-fit flex justify-end mt-[18vw] ml-[29vw]">
+                                        <div class="w-fit h-fit">
+                                            {{-- TEMBAK LINK EDIT (KE EXTRAPAGE VRERSI EDIT) --}}
+                                            <a href="">
+                                                <img class="w-[2.5vw] h-[2.5vw] hover:scale-[1.1]" src="{{asset('Assets/edit.png')}}" alt="">
+                                            </a>
+                                        </div>
+                                        <div class="w-[2.5vw] h-[2.5vw]">
+                                            {{-- TEMBAK CHAT --}}
+                                            <a href="">
+                                                <img class="ml-[1.5vw] w-[2.5vw] h-[2.5vw] hover:scale-[1.1]" src="{{asset('Assets/chat.png')}}" alt="">
+                                            </a>
                                         </div>
                                     </div>
                                 </div>
-                                <div class="absolute w-fit h-fit flex justify-end mt-[18vw] ml-[29vw]">
-                                    <div class="w-fit h-fit">
-                                        {{-- TEMBAK LINK EDIT (KE EXTRAPAGE VRERSI EDIT) --}}
-                                        <a href="">
-                                            <img class="w-[2.5vw] h-[2.5vw] hover:scale-[1.1]" src="{{asset('Assets/edit.png')}}" alt="">
-                                        </a>
+                            @else
+                                {{-- INI TEMPLATE KALAU CUMA ANGGOTA ESKUL --}}
+                                <div class="flex flex-col">
+                                    <div class="xtraboxcontainer flex justify-center items-center">
+                                        <div class="mr-[0.5vw] xtrabox flex justify-center items-center">
+                                            <img src="{{asset('Assets/$xtra->logo') }}" alt="{{ $xtra->name }}">
+                                        </div>
+                                        <div class="ml-[0.5vw] xtrabox flex flex-col items-start justify-center font-nunito leading-[3vw]">
+                                            <div class="text-[1.9vw] underline font-extrabold mb-[1vw]">{{ $xtra->name }}</div>
+                                            <div class="leading-[2vw] text-[1.65vw] font-semibold">
+                                                <div class="text-[1.6vw] font-semibold mb-[0.5vw]">{{ implode(' ', array_slice(explode(' ', optional(optional($xtra->leader)->userXmas)->name), 0, 2)) }}</div>
+                                                @if ($xtra->leader === NULL)
+                                                    <div class="text-[1.6vw] font-semibold mb-[0.5vw]">No Leader Yet</div>
+                                                @endif
+                                                <div class="text-[1.6vw] font-semibold mb-[0.5vw]">{{ date('D', strtotime($xtra->latest_schedule?->date)) . ', ' . date('H.i', strtotime($xtra->latest_schedule?->timeStart)) . ' - ' . date('H.i', strtotime($xtra->latest_schedule?->timeEnd)) }}</div>
+                                                <div class="text-[1.6vw] font-semibold mb-[0.5vw]">{{ $xtra->latest_schedule?->location }}</div>
+                                                @if ($xtra->latest_schedule === NULL)
+                                                    <div class="text-[1.6vw] font-semibold mb-[0.5vw]">No Schedule Yet</div>
+                                                @endif
+                                            </div>
+                                        </div>
                                     </div>
-                                    <div class="w-[2.5vw] h-[2.5vw]">
-                                        {{-- TEMBAHKCHAT --}}
-                                        <a href="">
-                                            <img class="ml-[1.5vw] w-[2.5vw] h-[2.5vw] hover:scale-[1.1]" src="{{asset('Assets/chat.png')}}" alt="">
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </a>
-                        @endif
-                        {{-- INI TEMPLATE KALAU CUMA ANGGOTA ESKUL --}}
-                        <a href="/xtralist">
-                            <div class="flex flex-col">
-                                <div class="xtraboxcontainer flex justify-center items-center">
-                                    <div class="mr-[0.5vw] xtrabox flex justify-center items-center">
-                                        <img src="{{asset('Assets/RunningLogo.png')}}" alt="">
-                                    </div>
-                                    <div class="ml-[0.5vw] xtrabox flex flex-col items-start justify-center font-nunito leading-[3vw]">
-                                        <div class="text-[1.9vw] underline font-extrabold mb-[1vw]">Running</div>
-                                        <div class="leading-[2vw] text-[1.65vw] font-semibold">
-                                            <div class="text-[1.6vw] font-semibold mb-[0.5vw]">Zakaria</div>
-                                            <div class="text-[1.6vw] font-semibold mb-[0.5vw]">Wed, 17.00 - 19.00</div>
-                                            <div class="text-[1.6vw] font-semibold mb-[0.5vw]">RTB</div>
+                                    <div class="absolute w-fit h-fit flex justify-end mt-[18vw] ml-[33vw]">
+                                        <div class="w-[2.5vw] h-[2.5vw]">
+                                            {{-- TEMBAK LINK CHAT DISINI --}}
+                                            <a href="">
+                                                <img class=" w-[2.5vw] h-[2.5vw] hover:scale-[1.1]" src="{{asset('Assets/chat.png')}}" alt="">
+                                            </a>
                                         </div>
                                     </div>
                                 </div>
-                                <div class="absolute w-fit h-fit flex justify-end mt-[18vw] ml-[33vw]">
-                                    <div class="w-[2.5vw] h-[2.5vw]">
-                                        {{-- TEMBAK LINK CHAT DISINI --}}
-                                        <a href="">
-                                            <img class=" w-[2.5vw] h-[2.5vw] hover:scale-[1.1]" src="{{asset('Assets/chat.png')}}" alt="">
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
+                            @endif
                         </a>
                     @endforeach
                 @else
