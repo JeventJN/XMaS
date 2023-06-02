0fcf398f6609486d994012f8b7ac092b3ad76496
JeventJN <jeventjn@gmail.com>
2023-06-02 13:07:13 +0700

    JsReportForm

--- public/js/Ketua/reportform.js
+++ public/js/Ketua/reportform.js
@@ -9,37 +9,10 @@ hidemodal.addEventListener('click', closeModal);
 hidemodalno.addEventListener('click', closeModal);
 
 function openModal(){
-    const xtraname = document.getElementById('xtraname')
-    const leadername = document.getElementById('leadername')
     const reporttitle = document.getElementById('reporttitle')
     const reportdesc = document.getElementById('reportdesc')
     const photo = document.getElementById('photo')
     const reportdate = document.getElementById('reportdate')
-
-    if(xtraname.value.length < 1){
-        alert("Extracurricular Name must be filled.");
-        return false;
-    }
-
-    if(xtraname.value.length > 30){
-        alert("Extracurricular Name must be least than 30 characters.");
-        return false;
-    }
-
-    if(leadername.value.length < 1){
-        alert("Leader Name must be filled.");
-        return false;
-    }
-
-    if(leadername.value.length < 6){
-        alert("Leader Name can't be less than 6 characters.");
-        return false;
-    }
-
-    if(leadername.value.length > 25){
-        alert("Leader Name must be less than 25 characters.");
-        return false;
-    }
 
     if (reportdate.value == '')
     {
