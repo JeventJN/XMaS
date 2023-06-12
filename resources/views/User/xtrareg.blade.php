f77bbb56cd9387b3554ca21f035c65228f536f36
VanessaKwan <vanessakwan17@gmail.com>
2023-06-12 12:39:23 +0700

    Backend - Xtra Registration - Done

--- resources/views/User/xtrareg.blade.php
+++ resources/views/User/xtrareg.blade.php
@@ -26,37 +26,37 @@
         <div class="w-[34%] bg-[#7599C1] h-[41.7vw]"></div>
         <div class="absolute ml-[24vw] w-[61vw] h-[24vw] bg-[#395474] mt-[11vw] rounded-[1vw] flex flex-col items-center justify-center">
             <div class="w-[51vw] h-[18vw]">
-                <div class="mt-[1vw] w-[25vw] rounded-[0.3vw]">
+                <div class="mt-[1vw] w-fit rounded-[0.3vw]">
                     <form id="xtrareg" action="/xtrareg" method="POST" onsubmit="return eventsubmits(this)" autocomplete="off">
                         @csrf
                         {{-- <select name="xtrachs" class="bg-gray-50 border border-gray-300 border-[0.1vw] rounded-[0.3vw] text-gray-900 text-sm focus:border-blue-500 block w-[25vw] h-[2.5vw] p-[0.2vw] focus:text-black text-[2vw]" required> --}}
                         <div class="select-wrap" class="max-w-[25vw] min-w-[25vw] max-h-[2.5vw] min-h-[2.5vw] border-none">
-                            <select name="xtrachs" class="input bg-gray-50 text-gray-900 text-sm block max-w-[25vw] min-w-[25vw] max-h-[2.5vw] min-h-[2.5vw] text-[2vw] scrollbar-hide">
+                            <select id="xtrachs" name="xtrachs" class="input bg-gray-50 text-gray-900 text-sm block max-w-[25vw] min-w-[25vw] max-h-[2.5vw] min-h-[2.5vw] text-[2vw] scrollbar-hide">
                                 <div id="select-box" class="max-w-[25vw] min-w-[25vw] max-h-[2.5vw] min-h-[2.5vw] border-none">
-                                <option selected="false" disabled class="hidden">Choose one of your Xtra</option>
-                                @foreach ($xtras as $xtr)
-                                    <option class="max-w-[25vw] min-w-[25vw] max-h-[2.5vw] min-h-[2.5vw] text-[1.2vw] border-none" id="xtrachs" value="{{ $xtr->kdExtracurricular }}">{{$xtr->name}}</option>
-                                @endforeach
+                                    <option selected="false" class="hidden" >
+                                        Choose one of your Xtra
+                                    </option>
+                                    @foreach ($xtras as $xtr)
+                                        <option class="max-w-[25vw] min-w-[25vw] max-h-[2.5vw] min-h-[2.5vw] text-[1.2vw] border-none" name="choose" value="{{ $xtr->kdExtracurricular }}" {{ old('choose') == $xtr->name ? 'selected' : '' }}>{{$xtr->name}}</option>
+                                    @endforeach
+                                    <input type="hidden" name="user" value="{{Auth::User()->NIP}}">
                                 </div>
                             </select>
                         </div>
+                        <div class="text-white text-[1.8vw] mt-[5vw] font-nunito">
+                            What’s the reason you want to join this Xtra?
+                        </div>
+                        <div class="w-[35vw] h-[3vw] bg-[#1B2F45] rounded-[1vw] flex justify-center items-end mt-[1vw]">
+                            <div class="w-[31vw] border-b-[0.2vw] mb-[0.5vw] flex items-center">
+                                <input type="text" name="reason" id="reason" class="w-[100%] h-[1.8vw] bg-[#1B2F45] outline-none text-white text-[1.2vw]">
+                            </div>
+                        </div>
+                        <button class="absolute ml-[-8vw] flex w-[100%] justify-end mt-[1vw]">
+                            <div class="w-[11vw] h-[2.5vw] bg-[#1B2F45] rounded-[0.2vw] text-white flex justify-center items-center hover:bg-black hover:cursor-pointer text-[1.3vw] hover:font-bold font-nunito">Register</div>
+                        </button>
+                        <script src="{{asset('js/User/xtrareg.js')}}"></script>
                     </form>
                 </div>
-                <form id="xtrareg" action="/xtrareg" method="POST" onsubmit="return eventsubmits(this)" autocomplete="off">
-                    @csrf
-                    <div class="text-white text-[1.8vw] mt-[2vw] font-nunito">
-                        What’s the reason you want to join this Xtra?
-                    </div>
-                    <div class="w-[35vw] h-[3vw] bg-[#1B2F45] rounded-[1vw] flex justify-center items-end mt-[1vw]">
-                        <div class="w-[31vw] border-b-[0.2vw] mb-[0.5vw] flex items-center">
-                            <input type="text" name="reason" id="reason" class="w-[100%] h-[1.8vw] bg-[#1B2F45] outline-none text-white text-[1.2vw]">
-                        </div>
-                    </div>
-                    <button class="flex w-[100%] justify-end mt-[3vw]">
-                        <div class="w-[11vw] h-[2.5vw] bg-[#1B2F45] rounded-[0.2vw] text-white flex justify-center items-center hover:bg-black hover:cursor-pointer text-[1.3vw] hover:font-bold font-nunito">Register</div>
-                    </button>
-                    <script src="{{asset('js/User/xtrareg.js')}}"></script>
-                </form>
             </div>
         </div>
     </body>
