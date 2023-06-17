d697fbdfff936c9ae82a50ec1001524be7930d02
JeventJN <jeventjn@gmail.com>
2023-06-17 13:20:41 +0700

    ChangeCssLink

--- resources/views/Admin/reportlist.blade.php
+++ resources/views/Admin/reportlist.blade.php
@@ -7,7 +7,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;0,1000;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900;1,1000&display=swap" rel="stylesheet">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{asset('css/Non-User/xtralistNU.css')}}">
+    <link rel="stylesheet" href="{{asset('css/Non-User/xtralist.css')}}">
     <title>Report List</title>
   @vite('resources/css/app.css')
 </head>
