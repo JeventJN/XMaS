48187d247f9edf54a3638c02719bb0b0ded27e04
JeventJN <jeventjn@gmail.com>
2023-06-26 21:52:21 +0700

    Fix Button Edit My CLub

--- resources/views/User/myclub.blade.php
+++ resources/views/User/myclub.blade.php
@@ -212,11 +212,22 @@
                                                     @csrf
                                                     <input type="hidden" name="NIP" value="{{ $nip }}">
                                                     <input type="hidden" name="kdXtra" value="{{ $xtra->kdExtracurricular }}">
-                                                    <button type="submit">
+                                                    <div id="btnsbmt" class="hover:cursor-pointer">
                                                         <img class="w-[2.5vw] h-[2.5vw] hover:scale-[1.1]" src="{{asset('Assets/edit.png')}}" alt="">
-                                                    </button>
+                                                    </div>
+                                                    <button type="submit" class="hidden btnsumbit"></button>
                                                 </form>
                                             </div>
+                                            <script>
+                                                document.addEventListener('DOMContentLoaded', function() {
+                                                    var btnsbmt = document.getElementById('btnsbmt');
+                                                    var btnsubmit = document.querySelector('.btnsumbit');
+
+                                                    btnsbmt.addEventListener('click', function() {
+                                                        btnsubmit.click();
+                                                    });
+                                                });
+                                            </script>
                                             <div class="w-[2.5vw] h-[2.5vw]">
                                                 {{-- TEMBAK CHAT --}}
                                                 <a href="/chat">
