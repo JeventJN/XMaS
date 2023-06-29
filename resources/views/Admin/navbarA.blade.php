6d11dc16e79876c9a086bdf0829f9ba9c1531cdf
Caudrey <103728671+Caudrey@users.noreply.github.com>
2023-06-30 00:48:10 +0700

    link to home logo with js

--- resources/views/Admin/navbarA.blade.php
+++ resources/views/Admin/navbarA.blade.php
@@ -12,6 +12,11 @@
     <div class="z-40 fixed w-screen h-[5.25vw] bg-[#1B2F45] flex items-center opacity-80">
         <div class="content w-[48%]">
                 <img class="cursor-pointer w-[10vw] h-[5vw] ml-[1vw]" src="{{asset('Assets/LogoXMaS.png')}}" alt="">
+            <script>
+                document.getElementById("imageLink").addEventListener("click", function() {
+                    window.location.href = "/home"; // Replace with the desired URL
+                });
+            </script>
         </div>
         <div class="content w-[70%] font-nunito text-[1.5vw] text-white flex justify-center justify-between mr-[3vw]">
             <a class="home w-[10vw] h-[3.25vw] hover:bg-[#A1A9B2] hover:text-[#1B2F45] flex items-center justify-center rounded-[1.6vw] {{ (url()->current() == url('/home')) ? 'hidden' : '' }}" href="/home">
