86c81a420de40a2ee0a5007be36ec1535e983465
VanessaKwan <vanessakwan17@gmail.com>
2023-07-14 01:53:08 +0700

    Backend - Xtra Page Leader

--- resources/views/Xtrapage.blade.php
+++ resources/views/Xtrapage.blade.php
@@ -31,7 +31,7 @@
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet" />
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
 
-    <title></title>
+    <title>Xtrapage</title>
 
     @vite('resources/css/app.css')
 </head>
